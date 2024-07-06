a40d837a30f60467ae5fdf538e081ebfd69ee3c2
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-06 09:31:27 +0200

    Layout component moved to shared folder since all views will be using it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 // components
 import { BaseCardComponent } from '@/components/shared/base-card/base-card.component';
 import { BaseButtonComponent } from '@/components/shared/base-button/base-button.component';
-import { LayoutComponent } from '@/components/layout/layout.component';
+import { LayoutComponent } from '@/components/shared/layout/layout.component';
 // utils
 import { RouterOutlet } from '@angular/router';
 
