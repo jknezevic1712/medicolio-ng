e308ff9943a961bc7334f91273f3dc7a69e3b52f
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-06 09:29:52 +0200

    App layout fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 // components
 import { BaseCardComponent } from '@/components/shared/base-card/base-card.component';
 import { BaseButtonComponent } from '@/components/shared/base-button/base-button.component';
+import { LayoutComponent } from '@/components/layout/layout.component';
 // utils
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BaseCardComponent, BaseButtonComponent],
+  imports: [
+    RouterOutlet,
+    BaseCardComponent,
+    BaseButtonComponent,
+    LayoutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,17 @@
 import { Routes } from '@angular/router';
 // components
-import { LayoutComponent } from '@/components/layout/layout.component';
-import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        component: AppComponent,
-      },
-      {
-        path: '**',
-        component: PageNotFoundComponent,
-      },
-    ],
+    loadComponent: () => import('./app.component').then((m) => m.AppComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./page-not-found/page-not-found.component').then(
+        (m) => m.PageNotFoundComponent
+      ),
   },
 ];
