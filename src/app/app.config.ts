b3565449553064a8e22d8c43a7723aa93ead9acb
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-09 13:51:01 +0200

    Changes:
    Update to view transitions animation.
    Disabled animation on initial load.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,40 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideZoneChangeDetection,
+} from '@angular/core';
 // utils
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, Router, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+// types
+import type { IsActiveMatchOptions } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true,
+        onViewTransitionCreated: ({ transition }) => {
+          const router = inject(Router);
+          const targetUrl = router.getCurrentNavigation()!.finalUrl!;
+          // Skip the transition if the only thing
+          // changing is the fragment and queryParams
+          const config: IsActiveMatchOptions = {
+            paths: 'exact',
+            matrixParams: 'exact',
+            fragment: 'ignored',
+            queryParams: 'ignored',
+          };
+
+          if (router.isActive(targetUrl, config)) {
+            transition.skipTransition();
+          }
+        },
+      })
+    ),
     provideClientHydration(),
   ],
 };
