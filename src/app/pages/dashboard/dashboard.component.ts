a464bc844a4c9fa78f4908e1c0eddecaeac40a31
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-10 12:54:52 +0200

    Patients list with mock patient data added.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 // components
 import { BaseCardComponent } from '@app/components/shared/base-card/base-card.component';
 import { BaseButtonComponent } from '@app/components/shared/base-button/base-button.component';
 import { LayoutComponent } from '@app/components/shared/layout/layout.component';
 // utils
 import { RouterOutlet } from '@angular/router';
+import { Patient } from '@app/libs/types/General';
 
 @Component({
   selector: 'dashboard-page',
@@ -18,8 +19,47 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.sass',
 })
-export class DashboardComponent implements OnInit {
-  ngOnInit(): void {
-    // console.log('ENV ', process.env);
-  }
+export class DashboardComponent {
+  mockPatients: Patient[] = [
+    {
+      id: '1',
+      name: 'Luke',
+      email: 'luke@test.com',
+      pin: '1234567890',
+      diagnosis:
+        'This is one random diagnosis and it definitely does not mean anything useful.',
+      doctorId: '1',
+      prescribtions: [],
+    },
+    {
+      id: '2',
+      name: 'Angie',
+      email: 'angie@test.com',
+      pin: '1234567891',
+      diagnosis:
+        'This is one random diagnosis and it definitely does not mean anything useful.',
+      doctorId: '1',
+      prescribtions: [],
+    },
+    {
+      id: '3',
+      name: 'Carl',
+      email: 'carl@test.com',
+      pin: '1234567892',
+      diagnosis:
+        'This is one random diagnosis and it definitely does not mean anything useful.',
+      doctorId: '1',
+      prescribtions: [],
+    },
+    {
+      id: '4',
+      name: 'Louis',
+      email: 'louis@test.com',
+      pin: '1234567893',
+      diagnosis:
+        'This is one random diagnosis and it definitely does not mean anything useful.',
+      doctorId: '1',
+      prescribtions: [],
+    },
+  ];
 }
