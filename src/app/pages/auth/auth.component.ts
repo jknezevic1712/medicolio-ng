1fc007fd21cb4fb6a9fa4ef4dcb17716ee8051e4
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-07-10 11:30:51 +0200

    Added Auth page UI.

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -2,11 +2,40 @@ import { Component } from '@angular/core';
 // components
 import { LayoutComponent } from '@app/components/shared/layout/layout.component';
 import { BaseCardComponent } from '@app/components/shared/base-card/base-card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BaseButtonComponent } from '@app/components/shared/base-button/base-button.component';
+// utils
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'auth-page',
   templateUrl: 'auth.component.html',
-  imports: [LayoutComponent, BaseCardComponent],
+  styleUrl: 'auth.component.sass',
+  imports: [
+    LayoutComponent,
+    BaseCardComponent,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BaseButtonComponent,
+  ],
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLogin = true;
+
+  get authTypeSwitchText() {
+    return this.isLogin
+      ? "Don't have an account yet?"
+      : 'Already have an account?';
+  }
+
+  switchAuthType() {
+    this.isLogin = !this.isLogin;
+  }
+
+  handleSubmit(formData: NgForm) {
+    console.log('DATA ', formData);
+  }
+}
